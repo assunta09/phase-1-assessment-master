9d4b1e04c28fa4b81aff9f2dbb24bfece6f68dcd
Assunta Crux <email": null>
2016-08-15 21:51:48 -0700

    Try to get this working

--- part-2/sock_drawer.rb
+++ part-2/sock_drawer.rb
@@ -1,3 +1,6 @@
+require_relative 'sock'
+require_relative 'sock_matcher'
+
 class SockDrawer
   attr_reader :socks, :matcher
 
@@ -5,4 +8,29 @@ class SockDrawer
     @socks = args.fetch(:socks) { Array.new }
     @matcher = args.fetch(:matcher)
   end
+
+  def supply_match_for(sock_to_match)
+    match = @socks.select {|sock| sock.color == sock_to_match.color}
+    @socks.delete(match[0])
+    match[0]
+  end
+
+  def supply_one_pair_of_socks
+    h = Hash.new(0)
+    @socks.each { |sock| h[sock.color] += 1 }
+  end
 end
+
+
+#Driver Code
+
+
+first_white_sock = Sock.new(brand: "Nike", style: "quarter", color: "white")
+second_white_sock = Sock.new(brand: "Nike", style: "quarter", color: "white")
+first_red_sock = Sock.new(brand: "Nike", style: "quarter", color: "red")
+first_black_sock = Sock.new(brand: "Nike", style: "quarter", color: "black")
+socks = [first_white_sock, first_black_sock, first_red_sock, second_white_sock]
+matcher = SockMatcher.new
+drawer =SockDrawer.new(socks: socks, matcher: matcher)
+second_red_sock = Sock.new(brand: "Nike", style: "quarter", color: "red")
+p drawer.supply_one_pair_of_socks
