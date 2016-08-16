ee8ec2ff0500c4864b1a6d253994946d728b70e2
Assunta Crux <email": null>
2016-08-15 20:55:53 -0700

    Repair mistakes

--- part-1/road_trip.rb
+++ part-1/road_trip.rb
@@ -1,19 +1,12 @@
-class Segment
-  attr_reader :miles
-  def initialize(miles)
-    @miles = miles
-  end
-
-end
-
+require_relative 'segment'
 
 class RoadTrip
   attr_accessor :name
   attr_reader :segments
 
   def initialize(args = {})
-    @name = args.fetch(:name)
-    @segments = args.fetch(:segments)
+    @name = args[:name]
+    @segments = args[:segments]
   end
 
   def add_segment(new_segment)
@@ -21,21 +14,17 @@ class RoadTrip
   end
 
   def total_miles
-    @total_miles = @segments.inject {|sum, segment| sum.miles[:miles] + segment.miles[:miles] }
-    @total_miles
+    total_miles = @segments.inject {|sum, segment| sum.miles + segment.miles}
   end
 
   def average_miles_per_segment
     number_of_trips = @segments.length
-    average_miles_per_segment = @total_miles/number_of_trips
-    average_miles_per_segment
+    average_miles_per_segment = total_miles / number_of_trips
   end
-
 end
 
 #DRIVER CODE
-# segments = [Segment.new(miles: 500), Segment.new(miles: 750)]
-# road_trip = RoadTrip.new(name: 'Trip to SF', segments: segments)
-
-# p road_trip.total_miles
-# p road_trip.average_miles_per_segment
+segments = [Segment.new(miles: 500), Segment.new(miles: 750)]
+road_trip = RoadTrip.new(name: 'Trip to SF', segments: segments)
+p road_trip.total_miles
+p road_trip.average_miles_per_segment
