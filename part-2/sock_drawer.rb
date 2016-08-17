361c580dbaf4d1f30897d89306ed418ff82746ae
Assunta Crux <email": null>
2016-08-16 19:50:15 -0700

    add correct answers

--- part-2/sock_drawer.rb
+++ part-2/sock_drawer.rb
@@ -9,33 +9,42 @@ class SockDrawer
     @matcher = args.fetch(:matcher)
   end
 
-  def supply_match_for(sock_to_match)
-    match = @socks.select {|sock| sock.color == sock_to_match.color}
-    @socks.delete(match[0])
-    match[0]
+  def supply_match_for(sock_1)
+    sock_match = @socks.select {|sock_2| @matcher.match?(sock_1, sock_2)}
+    @socks.delete(sock_match[0])
+    sock_match[0]
   end
 
   def supply_one_pair_of_socks
-    result = @socks.group_by {|sock| sock.color }.select {|key, value| value.size > 1}
-    @socks.delete_if {|sock| sock.color }
-    result.values[0]
+    matching_pair = ''
+    i = 1
+    while i < @socks.length
+      j = 0
+      while j < @socks.length
+        if @matcher.match?(@socks[i], @socks[j])
+          matching_pair = [@socks[i], @socks[j]]
+          @socks.delete(@socks[i])
+          @socks.delete(@socks[j])
+        end
+        j+=1
+      end
+      i += 1
+    end
+    matching_pair
   end
+
 end
 
 
-#Driver Code
 
 
-# first_white_sock = Sock.new(brand: "Nike", style: "quarter", color: "white")
-# second_white_sock = Sock.new(brand: "Nike", style: "quarter", color: "white")
-# first_red_sock = Sock.new(brand: "Nike", style: "quarter", color: "red")
-# first_black_sock = Sock.new(brand: "Nike", style: "quarter", color: "black")
-# socks = [first_white_sock, first_black_sock, first_red_sock, second_white_sock]
-# matcher = SockMatcher.new
-# drawer =SockDrawer.new(socks: socks, matcher: matcher)
-# second_red_sock = Sock.new(brand: "Nike", style: "quarter", color: "red")
+first_white_sock = Sock.new(brand: "Nike", style: "quarter", color: "white")
+second_white_sock = Sock.new(brand: "Nike", style: "quarter", color: "white")
+first_red_sock = Sock.new(brand: "Nike", style: "quarter", color: "red")
+first_black_sock= Sock.new(brand: "Nike", style: "quarter", color: "black")
 
-
-# non_matching_socks = [first_white_sock, first_red_sock]
-# drawer_with_no_matching_socks = SockDrawer.new(socks: non_matching_socks, matcher: matcher)
-# p drawer_with_no_matching_socks.supply_one_pair_of_socks
+socks = [first_white_sock, first_black_sock, first_red_sock, second_white_sock]
+matcher = SockMatcher.new
+drawer = SockDrawer.new(socks: socks, matcher: matcher)
+second_red_sock = Sock.new(brand: "Nike", style: "quarter", color: "red")
+p drawer.supply_one_pair_of_socks
